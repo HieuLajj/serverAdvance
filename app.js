a7d3b469e3daff94ee9caedcb990f735c10c7084
tomatowet <63393556+tomatowet@users.noreply.github.com>
2022-08-24 16:03:31 +0700

    html to image resize

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const sharp = require('sharp');
+const fs = require('fs');
+
+const nodeHtmlToImage = require('node-html-to-image')
+const cloudinary = require("./utils/cloudinary");
+
+app.use(express.static("./public"));
+app.use("/images", express.static('images'));
+app.set("view engine","ejs");
+app.set("views","./views");
+
+
+const blueResume = require("./docs/blue-resume");
 
 require('dotenv').config();
 
@@ -14,8 +27,56 @@ mongoose.connect(process.env.MONGO_URI,(e)=>{
 });
 
 app.get('/',(req,res)=>{
-    res.send('Hello');
+    res.render("trangchu")
 })
+app.get('/preViewResume',async(req,res)=>{  
+    // try {
+    //     console.log("ok");
+    //     const result = await cloudinary.uploader.upload("./images/image.png");
+    //     res.json(result);
+    //     console.log("xong");       
+    // } catch (error) {
+    //     console.log("loi")
+    //     console.log(error)
+    // }
+
+
+    // let pathImage = `./images/image${Date.now()}toHieulajj.png`;
+    // nodeHtmlToImage({
+    //     output: pathImage,
+    //     html: blueResume(),
+    //     content: { name: 'you' }
+    //   })
+    //     .then(() => 
+    //     {
+    //         //console.log(`http:/localhost:8000${pathImage.slice(-(pathImage.length-1)) }`)
+           
+    //     }
+    //     );     
+    let pathImage = `./images/image${Date.now()}toHieulajj.png`;
+    let pathImageChange = `./images/image${Date.now()}toHieulajj1.png`
+    nodeHtmlToImage({
+        output: pathImage,
+         html: blueResume(),
+         content: { name: 'you' }
+    }).then(() => 
+        {
+            console.log("thanh cong buoc 1");
+            sharp(pathImage).extract({ width: 586, height: 755, left:0,top:0,right:169 }).toFile(pathImageChange).then(
+               ()=>{
+                fs.unlink(pathImage, (err) => {
+                    if (err) {
+                    console.error(err)
+                    return
+                }else{
+                    console.log("xoa file cu thanh cong");
+                }
+                }) 
+               } 
+            );                   
+        }
+        );           
+});
 
 
 app.listen(8000,()=>{
---
+++ utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: 'hieulajj',
+    api_key: '893468955792623',
+    api_secret: 'mqXqRPVQGxf8q0tQrzyXRvCw1DA'
+
+});
+module.exports = cloudinary;
